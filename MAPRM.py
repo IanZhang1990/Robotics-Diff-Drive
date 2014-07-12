b73580a401b42779c8f0c837792729274a53a0e0
Yinan Zhang <ian.zhang1990@gmail.com>
2014-07-11 23:48:09 -0400

    iso cost

--- MAPRM.py
+++ MAPRM.py
@@ -3,14 +3,20 @@ from pygame.locals import *
 import sys, os, time
 import math, copy
 import random
+import Queue
 
 from geometry import *
 from obstacles import *
 from utility import *
 
+class Helper:
+	def __init__(self):
+		self.idx = 0.0;
+
 class LineSearch:
 	def __init__( self, start, dir ):
 		self.start = start;
+		self.idx = 0;
 		if dir:
 			self.dir   = dir.normalize();
 		pass;
@@ -46,10 +52,11 @@ class LineSearch:
 	def search(self, obstMgr, world, surf=None):
 		lastNear, lastDist  = self.nearPoint( self.start, obstMgr, world );
 		thisNear = copy.copy(lastNear); thisDist = lastDist;
-		idx      = 0;
+		helper = Helper();
+		pnt = copy.copy(self.start);
 		while( lastNear == thisNear ):
 			#lastDist = thisDist;	lastNear = thisNear;
-			pnt = v2( self.start.x+self.dir.x*idx, self.start.y+self.dir.y*idx )
+			pnt = v2( self.start.x+self.dir.x*helper.idx, self.start.y+self.dir.y*helper.idx )
 			if surf:
 				pygame.draw.line( DISPLAYSURF, (100, 100, 100), (int(pnt.x), int(pnt.y)), (int(pnt.x), int(pnt.y)), 1 );
 				for event in pygame.event.get():
@@ -61,8 +68,38 @@ class LineSearch:
 			#print '-------'
 			#print "{0}\t{1}\t{2}".format(pnt, lastNear, thisNear);
 			
-			idx += 1;
+			helper.idx += 1;
 		return pnt, thisDist;
+
+	def searchISO(self, obstMgr, world, cost, surf=None):
+		lastNear, lastDist  = self.nearPoint( self.start, obstMgr, world );
+		helper = Helper();
+		helper.idx = 0.0;
+		pnt = copy.copy(self.start);
+		acc = 1.0;
+
+		while( math.fabs(cost-lastDist) >= 1 ):
+			if( helper.idx > (cost/acc) ):
+				acc = acc / 2.0;
+				helper.idx = 0.0;
+				if acc <= 0.25:
+					break;
+
+			#lastDist = thisDist;	lastNear = thisNear;
+			pnt = v2( self.start.x+self.dir.x*helper.idx*acc, self.start.y+self.dir.y*helper.idx*acc )
+			if surf:
+				pygame.draw.line( DISPLAYSURF, (100, 100, 100), (int(pnt.x), int(pnt.y)), (int(pnt.x), int(pnt.y)), 1 );
+				for event in pygame.event.get():
+					pass;
+    			pygame.display.update();
+    			#time.sleep( .1 );
+
+			lastNear, lastDist = self.nearPoint( pnt, obstMgr, world );
+			#print '-------'
+			#print "{0}\t{1}\t{2}".format(pnt, lastNear, lastDist);
+			
+			helper.idx += 1;
+		return pnt, lastDist;
 
 
 class MAPRMSampler:
@@ -100,6 +137,98 @@ class MAPRMSampler:
 		progressBar(100);
 
 		return samples;
+
+	def sampleISO(self, n, cost, showProgressbar = True):
+		'''Sample configs with the same cost.'''
+		samples = [];
+		while( len(samples) < n ):
+			rand_x = random.randint( 0, self.world.WIDTH );
+			rand_y = random.randint( 0, self.world.HEIGHT);
+			pnt = v2(rand_x, rand_y);
+			start = None; end = None;
+			while( self.obstMgr.inside( pnt ) ):
+				rand_x = random.randint( 0, self.world.WIDTH );
+				rand_y = random.randint( 0, self.world.HEIGHT);
+				pnt = v2(rand_x, rand_y);
+			# Now, pnt is in free space
+			near, dist = self.nearPoint(pnt);
+			if dist > cost:
+				if dist/2.0 > cost:
+					start = near;
+					dirt = v2( pnt.x-near.x, pnt.y-near.y );
+				else:
+					start = pnt;
+					dirt = v2( near.x-pnt.x, near.y-pnt.y );
+			else:
+				start = pnt;
+				dirt  = v2( pnt.x-near.x, pnt.y-near.y ); 
+			#print "Last Near: {0}".format(near);
+			if dirt.r() == 0:
+				continue;
+
+			searcher       = LineSearch( start, dirt );
+			pntOnMA, dist  = searcher.searchISO( self.obstMgr, self.world, cost );
+			if math.fabs(dist-cost) <= 1: 
+				samples.append( (pntOnMA, dist) );
+			if showProgressbar:
+				progressBar( (float(len(samples))/float(n)) * 100 );
+		if showProgressbar:
+			progressBar(100);
+
+		return samples;		
+
+	def sampleISOn( self, cost ):
+		'''Sample on the iso-cost contour such that no sample is centered in existing ones'''
+		def inside(point, center, radius):
+			distSqr = (point.x-center.x)**2 + (point.y-center.y)**2;
+			if distSqr < radius**2:
+				return True;
+			else:
+				return False;
+
+		def getNewSample( point, dist ):
+			'''Get points on the boundary of the disc that has the same cost as the center'''
+			retSamps = [];
+			for idx in range(0, 360):
+				pnt = v2( point.x+float(dist)*math.cos(idx), point.y+float(dist)*math.sin(idx) );
+				near, currDist = self.nearPoint(pnt);
+				if math.fabs( dist-currDist ) <= 0.5:
+					retSamps.append( pnt );
+
+			return retSamps;
+
+
+		samples = [];
+		que = Queue.Queue();
+		firstSample = self.sampleISO( 1, cost, False );
+		samples.append(firstSample[0][0]);
+		que.put( firstSample[0][0] );
+
+		while not que.empty():
+			sample = que.get();
+			print sample;
+			newSamps = getNewSample( sample, cost );
+			if len(samples) > 2:
+				ifwithin = False;
+				for newsamp in newSamps:
+					for j in range(1, len(samples)):
+						if inside(newsamp, samples[j], cost):
+							ifwithin = True;
+							break;
+					if not ifwithin:
+						samples.append( newsamp );
+						que.put( newsamp );
+						clear = lambda: os.system('clear');
+    					clear();
+    					print len(samples);
+    		else:
+    			for newsamp in newSamps:
+    				samples.append(newsamp);
+    				que.put(newsamp);
+    				print que.empty();
+
+		return samples;
+
 
 class World:
     '''World class'''
@@ -151,7 +280,7 @@ gameWorld.render( DISPLAYSURF );
 #pygame.image.save( DISPLAYSURF, 'world.PNG' );
 
 '''
-testPt = v2( 10, 300 );
+testPt = v2( 772.000,795.000 );
 start = None; end = None;
 if not gameWorld.obstMgr.inside( testPt ):
 	print testPt
@@ -160,19 +289,27 @@ if not gameWorld.obstMgr.inside( testPt ):
 	start = testPt;
 	dirt  = v2( testPt.x-near.x, testPt.y-near.y );
 searcher  = LineSearch( start, dirt );
-pntOnMA, dist = searcher.search( gameWorld.obstMgr, gameWorld, DISPLAYSURF );
+pntOnMA, dist = searcher.searchISO( gameWorld.obstMgr, gameWorld, 30, DISPLAYSURF );
+'''
+#samples = sampler.sampleISO( 1000, 30 );
+#samples = sampler.sample( 1000 );
+samples = sampler.sampleISOn(30);
 
 '''
-samples = sampler.sample( 1000 );
-
 for sample in samples:
 	pygame.draw.circle( DISPLAYSURF, (220, 220, 220), (int(sample[0].x), int(sample[0].y)), int(sample[1]) );
 	
 for sample in samples:
 #	pygame.draw.circle( DISPLAYSURF, (30, 30, 30), (int(sample[0].x), int(sample[0].y)), int(sample[1]), 1 );
     pygame.draw.line( DISPLAYSURF, (30, 30, 30), (int(sample[0].x), int(sample[0].y)),(int(sample[0].x), int(sample[0].y)),2 );
-	
-gameWorld.render(DISPLAYSURF);
+'''
+
+
+for sample in samples:
+#	pygame.draw.circle( DISPLAYSURF, (30, 30, 30), (int(sample[0].x), int(sample[0].y)), int(sample[1]), 1 );
+    pygame.draw.line( DISPLAYSURF, (30, 30, 30), (int(sample.x), int(sample.y)),(int(sample.x), int(sample.y)), 5 );
+
+#gameWorld.render(DISPLAYSURF);
 
 #pygame.draw.circle( DISPLAYSURF, (100, 100, 100), (int(pntOnMA.x), int(pntOnMA.y)), 1 );
-pygame.image.save( DISPLAYSURF, 'MediaAxis_Cover.PNG' );
+pygame.image.save( DISPLAYSURF, 'MediaAxis_cost_30_bnd.PNG' );
