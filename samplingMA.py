828ed406a1bf45ea6d0318a3ad952c145ca5aa16
Yinan Zhang <ian.zhang1990@gmail.com>
2014-07-24 05:35:10 -0400

    1

--- samplingMA.py
+++ samplingMA.py
@@ -39,12 +39,13 @@ class LineSearch:
 		@param pnt: config point in c-space.  mapped. v3()'''
 		config_ = Config( v2(pnt.x, pnt.y), pnt.z );
 		config  = self.mapper.unmap(config_);
-		self.robot.setConfig(config_);
+		self.robot.setConfig(config);
 		timeclearance = self.obstMgr.time2obsts( self.robot, self.mapper );
 		return timeclearance;
 
 	def search(self):
 		'''search for the point in the medial axis'''
+		#print "\n=====================\nsearching..."
 		t = 1;
 		last_increse = False;
 		last_dist = self.dist2Obst(self.start);
@@ -52,6 +53,7 @@ class LineSearch:
 			temp = self.start + self.dir*t;
 			this_dist = self.dist2Obst(temp);
 			this_increase = ((this_dist-last_dist)>0);
+			#print this_dist;
 			if last_increse and not this_increase:
 				if not self.obstMgr.intersects(self.robot):
 					return temp, this_dist;
@@ -78,7 +80,7 @@ class Sphere:
 
 	def contains( self, point ):
 		'''determine if the sphre contains a point'''
-		dist = dist = (point - self.center).r();
+		dist = (point - self.center).r();
 		return dist <= self.radius;
 
 	def isClosed(self):
@@ -133,40 +135,14 @@ class SamplerV2:
 
 	def dist2obsts(self, config):
 		self.robot.setConfig(config);
-		return self.obstMgr.dist2obsts( self.robot );		l = 40;
-		maSamples = [];
-		for point in pntset:
-			#Check if within any discs.
-			good = True;
-			for disc in maSamples:
-				if disc.add( point ):
-					good = False;
-					#pntset.remove( point );
-					break;
-
-			if not good:
-				continue;
-
-			dir_x = random.randint( -100, 100 );
-			dir_y = random.randint( -100, 100);
-			if dir_x == dir_y == 0:
-				dir_x = dir_y = 50;
-			rand_dir = v2(dir_x, dir_y);
-			line_searcher = LineSearch( point, rand_dir, l, self.obstMgr, self.world );
-			maSample = line_searcher.search();
-			if maSample[0] is not None and maSample[1] >= 5:
-				newDisc = Disc( maSample[0], maSample[1] );
-				maSamples.append( newDisc );
-				#pntset.remove( point );
-
-		self.MASamples = copy.copy(maSamples);
+		return self.obstMgr.dist2obsts( self.robot );
 
 	def getSphere( self, config, clearance ):
 		'''Assume there is T clearance, T == @clearance, at configuration @config, 
 		get the sphere at that config'''
 		radius = clearance / ( math.sqrt(2) );
 		config_ = self.mapper.map(config);
-		center = [ config_.x, config_.y, config_.orient ];
+		center = v3(config_.x, config_.y, config_.orient);
 		return Sphere( center, radius );
 
 	def save_data( self, filename ):
@@ -209,7 +185,7 @@ class SamplerV2:
 		'''Sample spheres centered on Medial Axis in c-space.
 		@param pntset: A set of random points in free space. Coordinates are mapped.
 		@param rho   : number of points per unit volume.    '''
-		l = 40;
+		l = 15;
 		self.maSamples = [];
 		for point in pntset:
 			#Check if within any balls.
@@ -225,14 +201,16 @@ class SamplerV2:
 
 			dir_x = random.randint( -100, 100 );
 			dir_y = random.randint( -100, 100 );
-			dir_z = random.randint( -100, 100 );
+			dir_z = 0; #random.randint( -100, 100 );
 			if dir_x == dir_y == dir_z == 0:
 				dir_x = dir_y = dir_z = 50;
 			rand_dir = v3(dir_x, dir_y, dir_z);
 			line_searcher = LineSearch( point, rand_dir, l, self.obstMgr, self.robot, self.mapper );
 			maSample = line_searcher.search();
-			if maSample[0] is not None and maSample[1] >= 5:
-				newBall = Sphere( maSample[0], maSample[1] );
+			if maSample[0] is not None and maSample[1] >= 0.01:
+				config_ = Config( v2(maSample[0].x, maSample[0].y), maSample[0].z );
+				config  = self.mapper.unmap(config_);
+				newBall = self.getSphere(config, maSample[1]);
 				self.maSamples.append( newBall );
 				self.partition.addSphere( newBall );
 
@@ -244,9 +222,9 @@ class SamplerV2:
 		pntset = [];
 		for i in range(0, n):
 			#progressBar( float(len(pntset)) / n * 100);
-			rand_x = random.randint( 0, self.world.WIDTH );
-			rand_y = random.randint( 0, self.world.HEIGHT);
-			rand_theta = random.uniform( 0, 2*math.pi );
+			rand_x 	   = random.randint( 0, self.world.WIDTH );
+			rand_y 	   = random.randint( 0, self.world.HEIGHT);
+			rand_theta = 0;#random.uniform( 0, 2*math.pi );
 
 			config  = Config(v2(rand_x, rand_y), rand_theta);
 			self.robot.setConfig( config );
